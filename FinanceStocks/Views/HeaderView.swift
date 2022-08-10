1281cf303b3aa2086fe69f7a95c4cd2f1a634b3c
Mike19950326 <zth19950326@yahoo.com>
2022-08-10 17:46:51 +0800

    Create Project

---
+++ FinanceStocks/Views/HeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  HeaderView.swift
+//  FinanceStocks
+//
+//  Created by 蜡笔小新的动感超人Imac on 2022/8/10.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView()
+    }
+}
