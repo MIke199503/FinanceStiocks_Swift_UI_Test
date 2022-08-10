0bf1997ba8c95bcb9e4f1b7fd02de16e02b4f515
Mike19950326 <zth19950326@yahoo.com>
2022-08-10 17:21:30 +0800

    Initial Commit

---
+++ FinanceStocks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FinanceStocks
+//
+//  Created by 蜡笔小新的动感超人Imac on 2022/8/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FinanceStocks/FinanceStocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceStocksApp.swift
+//  FinanceStocks
+//
+//  Created by 蜡笔小新的动感超人Imac on 2022/8/10.
+//
+
+import SwiftUI
+
+@main
+struct FinanceStocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
